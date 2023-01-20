97cc1661a5fb731eb2c774c609cfc2decad8ea4d
Antonio Navarro <antonio.navarro@iesnervion.es>
2023-01-20 10:21:51 +0100

    Método para imprimir un Array 2d agregado

--- src/ejercicios/Methods.java
+++ src/ejercicios/Methods.java
@@ -4,5 +4,11 @@ package ejercicios;
 import java.util.Arrays;
 
 public class Methods {
-    
+    public static void printArray(int array[][]) {
+        for (int fila = 0; fila < array.length; fila++) {
+            for (int columna = 0; columna < array[0].length; columna++) {
+                System.out.print(array[fila][columna] + " ");
+            } System.out.println();
+        }
+    }
 } // € Hecho por Antonio Navarro
