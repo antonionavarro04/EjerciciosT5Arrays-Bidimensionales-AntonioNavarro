94dee2e9fb6d68b1e330442870119348748fde97
Antonio Navarro <antonio.navarro@iesnervion.es>
2023-02-06 09:29:35 +0100

    Ejercicio 2 Corregido

--- src/ejercicios/Ejercicio3.java
+++ src/ejercicios/Ejercicio3.java
@@ -5,75 +5,49 @@ import java.util.Scanner;
 import java.util.Locale;
 
 public class Ejercicio3 {
-    public static void printArray(String array[][]) { // * Modificado para este caso
-        for (int fila = 0; fila < array.length; fila++) {
-            if (fila == 0) {
-                System.out.print("Alumno:\t");
-            } else {
-                System.out.print("Nota:\t");
-            }
-            for (int columna = 0; columna < array[0].length; columna++) {
-                if (fila == 0) {
-                    System.out.print(array[fila][columna] + "\t");
-                } else {
-                    System.out.print(array[fila][columna] + "\t\t");
-                }
-            } System.out.println();
-        }
-    }
-
     // ^ Definimos una instancia de la clase Scanner como private static final de nombre read y cambiamos su formato a US
     private static final Scanner read = new Scanner(System.in).useLocale(Locale.US);
 
     public static void main(String[] args) {
-        // ^ Definimos un array de 2x4
-        String array[][] = new String[2][4];
-
-        // ! Rellenamos la primera fila del Array con los nombres
-        for (int pos = 0; pos < array[0].length; pos++) {
-            String s = String.valueOf(pos + 1);
-            array[0][pos] = "Alumno " + s;
-        } 
+        // ^ Definimos un array de 4*5
+        int array[][] = new int[4][5];
         
-        // ! Rellenamos la segunda fila del Array con las notas, preguntándole estas al usuario#
-        System.out.println("Introduce las notas de los Alumnos:");
-        for (int pos = 0; pos < array[1].length; pos++) {
-            String s = String.valueOf(pos + 1);
-            System.out.print("Alumno " + s + " >>> ");
-            array[1][pos] = read.nextLine();
-        }
-        
-        // ? Salto de Línea Triple
-        System.out.println("\n-------------------------------------------------------------------\n");
-
-        // ? Mostramos las notas
-        printArray(array);
-        
-        // ! Sacamos la nota mínima, máxima y media de las calificaciones, para facilitar esta tarea vamos a sacar las notas a un array entero
-        int aux[] = new int[4];
-        for (int pos = 0; pos < aux.length; pos++) {
-            aux[pos] = Integer.parseInt(array[1][pos]);
+        // ! Rellenamos el Array con las notas de los alumnos, cada fila será un alumno distinto
+        for (int fila = 0; fila < array.length; fila++) {
+            System.out.printf("Introduce las notas del Alumno %s:\n", fila + 1);
+            for (int columna = 0; columna < array[0].length; columna++) {
+                System.out.printf("Asignatura %s >>> ", columna + 1);
+                array[fila][columna] = read.nextInt();
+            } System.out.println();
         }
 
-        // ! Tanto para la nota mínima como la máxima vamos a recorrerlas con un foreach buscando estas notas
-        // ! Para la media vamos a recorrer el array e ir sumando los numeros en una variable media, al final lo dividiremos entre la longitud del array
-        // ! Todas las variables la inicializaremos con el primer valor del array y al recorrerlo empezaremos desde el segundo
-        int notaMinima = aux[0], notaMaxima = aux[0], notaMedia = aux[0];
-        for (int pos = 1; pos < aux.length; pos++) {
-            notaMedia += aux[pos]; // * Sumamos las notas
-            if (aux[pos] > notaMaxima) { // * Comprobamos si esta nota es mayor a la anterior
-                notaMaxima = aux[pos];
-            } if (aux[pos] < notaMinima) { // * Comprobamos si esta nota es menor a la anterior
-                notaMinima = aux[pos];
+        // ? Limpiamos el Búffer del Scanner
+        read.nextLine();
+        
+        // ? Salto de Línea Doble
+        System.out.println("--------------------------------------------------------------------\n");
+        
+        // ! Recorremos el bucle y por cada fila imprimiremos la información del alumno concreto
+        for (int fila = 0; fila < array.length; fila++) {
+            int max = array[fila][0];
+            int min = array[fila][0];
+            double media = array[fila][0];
+            System.out.printf("Para el Alumno %s:\n", fila + 1);
+            for (int columna = 1; columna < array[0].length; columna++) {
+                if (max < array[fila][columna]) {
+                    max = array[fila][columna];
+                } else if (min > array[fila][columna]) {
+                    min = array[fila][columna];
+                } media += array[fila][columna];
             }
+            System.out.printf("Su nota máxima fue: %s\n", max);
+            System.out.printf("Su nota mínima fue: %s\n", min);
+            System.out.printf("Su nota media fue: %s\n", media / (double) array[0].length);
+            System.out.print("Pulsa Intro para continuar... ");
+            read.nextLine();
+            System.out.println();
         }
-
-        // ! Dividimos la media entre la longitud del array
-        notaMedia /= aux.length;
-
-        // ? Mostramos las notas máximas, mínimas y la media
-        System.out.println("La nota mínima fue " + notaMinima);
-        System.out.println("La nota máxima fue " + notaMaxima);
-        System.out.println("La nota media fue " + notaMedia);
+        System.out.println("Calificaciones totales, cada fila es un alumno diferente:");
+        Methods.printArray(array);
     }
 } // € Hecho por Antonio Navarro
